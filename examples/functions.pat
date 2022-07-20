0ee6af6d4b664c7ec6ce8ff259372608534f6fbd
Daniel-Esteban <dani.este.bri@gmail.com>
2022-07-20 09:57:07 +0200

    Rearrange queries and add function highlight

--- examples/functions.pat
+++ examples/functions.pat
@@ -2,6 +2,8 @@ fn myfunc(u8 arg1, u8 arg2, u32 ... args) {
     return arg1 + arg2;
 };
 
+myfunc(arg1, arg2, arg3);
+
 while(a < 50 && check()) {
     if (x == 36.5)
         break;
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,77 +1,67 @@
+(identifier) @variable
+
+; Misc
+(comment) @comment
+(ERROR) @error
+[ "(" ")" "{" "}" "[[" "]]" ] @punctuation.bracket
+[ ";" "," ] @punctuation.delimiter
+(define (identifier)@constant.macro) @preproc
+(pragma (_) @parameter) @preproc
+
+; Constants
+(dollar) @constant.builtin
+(string_literal) @string
+(escape_sequence) @string.escape
+(character_literal) @character
+(number_literal) @number
+(boolean_literal) @boolean
+
+; Functions
+(function_definition name:(identifier) @function)
+(function_call name:(identifier) @function)
+(field_identifier) @field
+(this) @field
+
+; Keywords
+[ "if" "else" ] @conditional
+[ "while" ] @repeat
 [
-  "struct"
-  "union"
+  "struct" "union"
+  "break" "continue"
   "padding"
   "using"
-  "fn"
-  "break"
-  "continue"
-  "return"
   (parent)
   (endian_val)
 ] @keyword
 
+"fn" @keyword.function
+"return" @keyword.return
+
 [
- "!"
- "~"
-
- "="
-
- ":"
- "?"
-
- "+"
- "-"
- "*"
- "/"
- "%"
- ">>"
- "<<"
- "&"
- "|"
- "^"
- "=="
- "!="
- ">"
- "<"
- ">="
- "<="
- "&&"
- "||"
- "^^"
-
+ "!" "~"
+ "=" "+=" "-="
+ ":" "?"
+ "+" "-" "*" "/" "%"
+ ">>" "<<"
+ "&" "|" "^"
+ "==" "!="
+ ">" "<" ">=" "<="
+ "&&" "||" "^^"
  "@"
 ] @operator
 
-[ "(" ")" "{" "}" "[[" "]]" ] @punctuation.bracket
+"#include" @include
 
-[ ";" "," ] @punctuation.delimiter
-
-[ "if" "else" ] @conditional
-
-[ "while" ] @repeat
-
-(identifier) @variable
+(type_identifier) @type
+(using_definition name: (identifier) @type)
 (primitive_type) @type.builtin
 (endian_indicator) @type.builtin
-(type_identifier) @type
-(number_literal) @number
-(string_literal) @string
-(character_literal) @character
-(escape_sequence) @string.escape
-(boolean_literal) @boolean
 (attribute) @attribute
-(comment) @comment
-(dollar) @constant.builtin
-(pragma (_) @parameter) @keyword
-(include) @include
-(include
-  (system_lib_string
-    ["<" ">"] @punctuation.bracket
-  )@namespace
-)
-(define) @keyword
-(field_identifier) @field
-(this) @field
 
-(ERROR) @error
+; Variables
+
+; Text
+; Tags
+
+; Overrides
+(system_lib_string ["<" ">"] @string) @string
